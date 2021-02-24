5c284651320a017e81c0bac8467da0c0884894f6
皮皮_侠 <8256375+skin-xia@user.noreply.gitee.com>
2021-02-24 22:16:05 +0800

    测试

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+    <el-aside width="200px">
+      <el-menu
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <!-- 一级菜单栏 -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
+      <!-- 右侧边栏 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -16,4 +67,30 @@ export default {
 </script>
 
 <style scoped>
+.home-container{
+  height: 100%;
+}
+.el-header{
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+.el-header div{
+  display: flex;
+  align-items: center;
+}
+.el-header span{
+  margin-left: 15px;
+}
+.el-aside{
+  background-color: #333744;
+}
+.el-main{
+  background-color: #eaedf1;
+}
+
 </style>
